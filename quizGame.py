f93736a8e1c15ddb2ff3ecc85d5a2ba277726f59
gazpool <g.e.pool1@outlook.com>
2022-01-02 12:24:27 +0000

    Update quizGame.py

--- quizGame.py
+++ quizGame.py
@@ -3,15 +3,46 @@ print("Welcome to my animal quiz")
 play = input("Do you want to play? ")
 
 if play.lower() != "yes":
+	print("Okay, another time then...")
 	quit() 
 
 print("okay letsss go!")
 score = 0
+qCount = 0
 
 answer = input("What is the fastest animal on land? ")
 if answer.lower() == "cheetah":
 	print("Correct, well done!")
 	score += 1
 else: print("Incorrect, better luck next time")
+qCount += 1
+
+answer = input("What colour is an orangutan? ")
+if answer.lower() == "orange":
+	print("Correct, well done!")
+	score += 1
+else: print("Incorrect, better luck next time")
+qCount += 1
+
+answer = input("How many tentacles does an octopus have? ")
+if answer.lower() == "eight" or answer == "8": 
+	print("Correct, well done!")
+	score += 1
+else: print("Incorrect, better luck next time")
+qCount += 1
+
+
+perc = round(score/qCount*100,2)
+
+
+
+if qCount == score:
+		print("Congratulations! You got all the questions correct, give yourself a pat on the back")
+elif score == 0:
+		print("Epic fail!! You got 0 questions correct, go study some more")
+else: print (f"Good effort. You got {score} out of {qCount} ({perc}%) questions correct")
+
+
+
 
 
